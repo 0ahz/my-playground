a3db1737b6c60a333e8bd8ef38bd6c837f007c2d
0ahz <shuangoahz@gmail.com>
2024-07-12 17:29:28 +0800

    chore: up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex items-center gap-2 bg-gray-100">
-    <template v-for="route in routes" :key="route.pat">
+  <div v-if="!isInIframe" class="flex items-center gap-2 bg-gray-100">
+    <template v-for="route in routes" :key="route.path">
       <RouterLink v-slot="{ isActive }" :to="route.path">
         <span class="px-2" :class="[isActive ? 'font-bold' : '']">{{
-          route.name
+          route.path
         }}</span>
       </RouterLink>
     </template>
@@ -15,4 +15,6 @@
 
 <script setup>
 import { routes } from 'vue-router/auto-routes';
+
+const isInIframe = window !== window.parent;
 </script>
